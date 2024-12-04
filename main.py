59fecabaa479704bcc25fbb884dd285ee9b4fdc0
Oscar Karbin <karbin.oscar2002@gmail.com>
2024-12-04 18:52:05 +0100

    new model trained for 30 epochs

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11s.pt")
 
 # Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="config.yaml", epochs=1, imgsz=640)
+results = model.train(data="config.yaml", epochs=30, imgsz=640)
